9b39acc9a415e5abf3da88bc9abd682d12482d01
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2024-08-26 13:44:40 -0600

    added day counter

--- terraform.py
+++ terraform.py
@@ -1,17 +1,22 @@
 # TO DO
-# count days
 # add plants
 
 import pygame
 
 pygame.init()
+
+#game clock
 clock = pygame.time.Clock()
 start_ticks = pygame.time.get_ticks()
+days = 0
+game_hours = 0
 
 canvas = pygame.display.set_mode((800,600))
 pygame.display.set_caption("terraform")
 
 background = pygame.image.load("land.png")
+font = pygame.font.Font("PixelifySans-VariableFont_wght.ttf", 32)
+day_text = font.render("day " + str(days), False, (255,255,255))
 
 # define colours for different times of the day
 morning = (255, 238, 130)
@@ -25,20 +30,27 @@ exit = False
 while not exit:
     clock.tick(60)
     seconds = (pygame.time.get_ticks()-start_ticks)/1000
-    print(seconds)
-
-    # TO DO -> reset clock every "day"; count days
-    # for this stage of testing, 1 day equals 15 seconds
-    if int(seconds) == 15:
+    game_hours += 1
+    
+    # for this stage of testing, 1 day equals 15 seconds (900 frames)
+    if game_hours == 900:
         canvas.fill(morning)
-    elif int(seconds) == 5:
+        game_hours = 0
+        days += 1
+        day_text = font.render("day " + str(days), False, (255,255,255))
+        print(days)
+    elif game_hours == 300:
         canvas.fill(day)
-    elif int(seconds) == 10:
+    elif game_hours == 600:
         canvas.fill(evening)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
 
-    canvas.blit(background, (0,0))       
+    canvas.blit(background, (0,0))  
+    # TO DO -> create adaptive width
+    pygame.draw.rect(canvas, (0,0,0), pygame.Rect(45,45,95,50))
+    canvas.blit(day_text, (50, 50))    
+
     pygame.display.update()
