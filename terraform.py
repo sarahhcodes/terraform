c879b3f41a5697469d34440834c1aef424f55e69
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2024-08-29 12:31:03 -0600

    added plant type; added variable width to days

--- terraform.py
+++ terraform.py
@@ -1,7 +1,6 @@
 # TO DO
+# put day display into class
 # add z-index to plant class
-# add argument for images list to Plant constructor
-# add variable width to date display
 # change font for day counter (numbers are not as readable as what they should)
 # tidy up code w/main function
 
@@ -20,7 +19,7 @@ FPS = 60
 plant_width = 53
 plant_height = 122
 
-images = [
+white_flower = [
     pygame.image.load("temp_plant/plant1.png"),
     pygame.image.load("temp_plant/plant2.png"),
     pygame.image.load("temp_plant/plant3.png"),
@@ -32,30 +31,30 @@ images = [
 # plant sprite constructor
 class Plant(pygame.sprite.Sprite):
     # initalize constructor
-    #TO DO -> add argument for images list (to allow for other plants)
-    def __init__(self, x, y):
+    def __init__(self, plant_type, x, y):
         super().__init__()
         self.age = 0
-        self.image = images[self.age]
+        self.plant_type = plant_type
+        self.image = self.plant_type[self.age]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
     # update image according to the age of the plant
     def grow(self):
-        if self.age < len(images) - 1:
+        if self.age < len(self.plant_type) - 1:
             self.age += 1
-            self.image = images[self.age]
+            self.image = self.plant_type[self.age]
         else:
             self.age += 1
-            self.image = images[len(images) - 1]
+            self.image = self.plant_type[len(self.plant_type) - 1]
 
 # group for plants
 plants = pygame.sprite.Group()
 
 # initalize plants (for testing)
-plant1 = Plant(500,500)
+plant1 = Plant(white_flower, 500,500)
 plants.add(plant1)
-plant2 = Plant(600,300)
+plant2 = Plant(white_flower, 600,300)
 plants.add(plant2)
 
 CANVAS_WIDTH = 800
@@ -66,7 +65,10 @@ pygame.display.set_caption("terraform")
 
 background = pygame.image.load("land.png")
 font = pygame.font.Font("PixelifySans-VariableFont_wght.ttf", 32)
-day_text = font.render("day " + str(days), False, (255,255,255))
+# make into class?
+day_text = "day " + str(days)
+day_render = font.render(day_text, False, (255,255,255))
+day_width, day_height = font.size(day_text)
 
 # define colours for different times of the day
 morning = (255, 238, 130)
@@ -83,9 +85,10 @@ while not exit:
             exit = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-
+            # draw flower if mouse is clicked on ground
+            # TO DO -> allow for user to choose different plants
             if y > CANVAS_HEIGHT/2:
-                plants.add(Plant(x, y - (plant_height/2)))
+                plants.add(Plant(white_flower, x, y - (plant_height/2)))
 
     clock.tick(FPS)
     seconds = (pygame.time.get_ticks()-start_ticks)/1000
@@ -96,7 +99,9 @@ while not exit:
         canvas.fill(morning)
         game_hours = 0
         days += 1
-        day_text = font.render("day " + str(days), False, (255,255,255))
+        day_text = "day " + str(days)
+        day_render = font.render(day_text, False, (255,255,255))
+        day_width, day_height = font.size(day_text)
         print(days)
         
         # update plants
@@ -113,9 +118,8 @@ while not exit:
     canvas.blit(background, (0,0))  
 
     # draw day counter
-    # TO DO -> create adaptive width
-    pygame.draw.rect(canvas, (0,0,0), pygame.Rect(45,45,95,50))
-    canvas.blit(day_text, (50, 50))    
+    pygame.draw.rect(canvas, (0,0,0), pygame.Rect(45, 45, day_width + 15, day_height + 10))
+    canvas.blit(day_render, (50, 50))    
     
     plants.draw(canvas)
 
